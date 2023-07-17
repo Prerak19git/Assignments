eeb7fce0cc9bcc4a7f511e1c9e927aecedc1cf76
Prerak19git <139736040+Prerak19git@users.noreply.github.com>
2023-07-17 15:55:11 +0530

    Add files via upload

---
+++ task1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+awk '{print $9}' test.log | sort -u >unique_values.txt
+
+freq_of_200=$(awk '{print $9}' test.log | grep 200 | wc -l)
+
+echo "The number of HTTP 200 errors is $freq_of_200"
+
+while read -r line
+do
+     freq=$(awk '{print $9}' test.log | grep $line | wc -l)
+     echo "Error code $line has $freq occurances"
+   
+done < unique_values.txt
